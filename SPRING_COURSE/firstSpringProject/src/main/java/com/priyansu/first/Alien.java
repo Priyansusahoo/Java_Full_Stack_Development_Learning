4d2f55cd4fdeb03da12d2711e273567bde151108
Priyansu Sahoo <priyansusahoo1@gmail.com>
2023-12-29 17:06:46 +0530

    Added: Laptop CLASS
    Learned about Spring Boot AUTOWIRE, @Autowired

--- SPRING_COURSE/firstSpringProject/src/main/java/com/priyansu/first/Alien.java
+++ SPRING_COURSE/firstSpringProject/src/main/java/com/priyansu/first/Alien.java
@@ -1,10 +1,14 @@
 package com.priyansu.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+	@Autowired
+	Laptop lap;
 	public void code(){
-		System.out.println("I'm Coding...");
+//		System.out.println("I'm Coding...");
+		lap.compile();
 	}
 }
---
+++ SPRING_COURSE/firstSpringProject/src/main/java/com/priyansu/first/Laptop.java
@@ -0,0 +1,14 @@
+package com.priyansu.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		// TODO Auto-generated method stub
+		System.out.println("Compiling...");
+		
+	}
+
+}

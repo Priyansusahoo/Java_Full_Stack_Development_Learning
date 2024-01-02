aa00252187a1de881f3da4eafde2c6cb709ad46f
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-01-02 16:28:42 +0530

    Update HomeController.java
    Accepting User Input via @RequestParam

--- SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
+++ SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
@@ -2,25 +2,24 @@ package com.priyansu.springmvcboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
-	public String home() {
-//		System.out.println("Home Page requested...");
+	public String home() 
+	{
 		return "index.jsp";
-	}
-	
+	}	
 	@RequestMapping("add")
-	public String add(HttpServletRequest req) {
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
-		
+	public String add(@RequestParam("num1") int i, 
+			@RequestParam("num2") int j,
+			HttpSession session) 
+	{		
 		int num3 = i + j;
-		HttpSession session = req.getSession();
 		session.setAttribute("num3", num3);
 		return "result.jsp";
 	}

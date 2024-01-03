5c377fd7cadd0c16201a6f286b7086298635d00e
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-01-03 11:03:46 +0530

    Used Model in HomeController.java

--- SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
+++ SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
@@ -1,8 +1,8 @@
 package com.priyansu.springmvcboot;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +18,7 @@ public class HomeController {
 	}	
 	@RequestMapping("add")
 	public String add(@RequestParam("num1") double i, 
-			@RequestParam("num2") double j, ModelMap m) 
+			@RequestParam("num2") double j, Model m) 
 	{	
 		double num3 = i + j;
 		m.addAttribute("num", num3);

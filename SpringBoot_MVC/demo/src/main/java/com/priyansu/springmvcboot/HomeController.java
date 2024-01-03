ad287ea5abded60ed9b185d9a680f2ab6babac76
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-01-03 11:55:26 +0530

    Update HomeController.java
    Add RequestMapping for "addAlien"

--- SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
+++ SpringBoot_MVC/demo/src/main/java/com/priyansu/springmvcboot/HomeController.java
@@ -1,5 +1,6 @@
 package com.priyansu.springmvcboot;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //import org.springframework.ui.ModelMap;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.servlet.ModelAndView;
 //import jakarta.servlet.http.HttpServletRequest;
 //import jakarta.servlet.http.HttpSession;
+import com.priyansu.springmvcboot.model.Alien;
 
 @Controller
 public class HomeController {
@@ -22,6 +24,18 @@ public class HomeController {
 	{	
 		double num3 = i + j;
 		m.addAttribute("num", num3);
+		return "result";
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@RequestParam("aid") int aid, 
+			@RequestParam("aname") String aname, Model m)
+	{
+		Alien a = new Alien();
+		a.setAid(aid);
+		a.setAname(aname);
+		
+		m.addAttribute("alien", a);
 		return "result";
 	}
 }

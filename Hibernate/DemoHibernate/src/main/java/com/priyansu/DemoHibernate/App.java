c945d062780af27952a25b64263fde21926e6505
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-01-05 18:28:53 +0530

    Added: Hibernate
    Initialized Hibernate Project -> (Maven project quickstart)

---
+++ Hibernate/DemoHibernate/src/main/java/com/priyansu/DemoHibernate/App.java
@@ -0,0 +1,13 @@
+package com.priyansu.DemoHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
